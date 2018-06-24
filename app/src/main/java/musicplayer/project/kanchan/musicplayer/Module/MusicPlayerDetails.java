227d9848f14affb01866f166b365cae4a2d561ef
Kanchan <mailmekanchan90@gmail.com>
2018-06-24 11:50:44 +0530

    Commit on Design Done
    Design Done

--- app/src/main/java/musicplayer/project/kanchan/musicplayer/Module/MusicPlayerDetails.java
+++ app/src/main/java/musicplayer/project/kanchan/musicplayer/Module/MusicPlayerDetails.java
@@ -6,16 +6,21 @@ import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import java.io.File;
 import java.util.ArrayList;
 
 import musicplayer.project.kanchan.musicplayer.R;
 
-public class MusicPlayerDetails extends Activity {
+public class MusicPlayerDetails extends Activity implements View.OnClickListener {
     private ArrayList<File>songlist;
     private int selectedpos;
     private MediaPlayer mediaPlayer;
+    private TextView tv_play,tv_ff,tv_bb;
+    private SeekBar sb_seek;
     @Override
     protected void onStart() {
         super.onStart();
@@ -36,8 +41,24 @@ public class MusicPlayerDetails extends Activity {
         Bundle b = songReceive.getExtras();
         songlist = (ArrayList) b.getParcelableArrayList("songlist");
         selectedpos = b.getInt("pos");
+        initView();
         initiatingMediaPlayer();
 
+
+    }
+
+    private void initView() {
+        tv_play = (TextView)findViewById(R.id.tv_play);
+        tv_bb = (TextView)findViewById(R.id.tv_bb);
+        tv_ff = (TextView)findViewById(R.id.tv_ff);
+        sb_seek = (SeekBar)findViewById(R.id.sb_seek);
+        SetClickListener();
+    }
+
+    private void SetClickListener() {
+        tv_play.setOnClickListener(this);
+        tv_bb.setOnClickListener(this);
+        tv_ff.setOnClickListener(this);
 
     }
 
@@ -45,5 +66,10 @@ public class MusicPlayerDetails extends Activity {
         Uri mediapath = Uri.parse(songlist.get(selectedpos).toString());
         mediaPlayer = MediaPlayer.create(getApplicationContext(),mediapath);
         mediaPlayer.start();
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
